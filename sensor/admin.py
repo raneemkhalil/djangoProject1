f9b1cd85e06aaa01305174f2dd9a0e4b9ef6f8f3
raneemkhalil <raneem.alwazany@gmail.com>
2022-08-09 18:07:53 +0300

    test

--- sensor/admin.py
+++ sensor/admin.py
@@ -4,7 +4,13 @@ from . import models
 # Register your models here.
 @admin.register(models.PressureSensor)
 class PressureSensor(admin.ModelAdmin):
-    list_display = ('id', 'Label', 'InstallationDate', 'Latitude', 'Longitude')
+    list_display = ('id', 'Label', 'InstallationDate', 'Latitude', 'Longitude', 'PressureSensor_Id')
+    readonly_fields = ('PressureSensor_Id',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.PressureSensor_Id = request.user
+        super().save_model(request, obj, form, change)
 
 @admin.register(models.PressureReading)
 class PressureReading(admin.ModelAdmin):
--- sensor/models.py
+++ sensor/models.py
@@ -1,12 +1,23 @@
-from django.db import models
+import uuid
 
+import rest_framework.status
+from django.db import models
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def mustStartWithPSSR(value):
+    str = [*value]
+    if str[0] != 'P' and str[1] != 'S' and str[2] != 'S' and str[3] != 'R':
+        raise ValidationError('Value must start with PSSR', code=rest_framework.status.HTTP_400_BAD_REQUEST)
+
+
 class PressureSensor(models.Model):
 
-    Label = models.CharField(max_length=70)
+    Label = models.CharField(max_length=70, validators=[mustStartWithPSSR])
     InstallationDate = models.DateTimeField()
     Latitude = models.FloatField(default=100)
     Longitude = models.FloatField(default=100)
+    PressureSensor_Id = models.CharField(max_length=50, null=False, default=uuid.uuid4)
 
     def __str__(self):
         return self.Label
@@ -17,5 +28,5 @@ class PressureReading(models.Model):
     DateTime = models.DateTimeField()
     Value = models.FloatField(default=0)
 
-    def __str__(self):
-        return self.SensorId
+    # def __str__(self):
+    #     return self.SensorId
