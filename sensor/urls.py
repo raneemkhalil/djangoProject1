2c068513ed563f452c19de536408cedcd1f74620
raneemkhalil <raneem.alwazany@gmail.com>
2022-09-27 02:36:23 +0300

    task13

--- sensor/urls.py
+++ sensor/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from . import views
+from django.http import request
 
 urlpatterns = [
     path('api/pressure_sensors/', views.Sensors.as_view({'get': 'list', 'post': 'create'}), name='pressure_sensors'),
     path('api/pressure_readings/', views.Readings.as_view({'get': 'list', 'post': 'create'})),
     path('api/fun_calculate/', views.calculating_readings, name='calculate'),
     path('api/class_calculate/', views.Calculating.as_view(), name='calculation'),
+    path('api/greet/', views.greet_users, name='greeting'),
 ]
